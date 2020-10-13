37d1303305b4281dd7e2c6145ea12c648b30d32b
charmeemehta01 <charmeemehta01@gmail.com>
2020-10-13 17:02:58 +0530

    Attempts displayed

--- trendybucket.sql
+++ trendybucket.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.8.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 13, 2020 at 12:36 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.10
+-- Generation Time: Oct 13, 2020 at 01:32 PM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 7.2.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -24,14 +25,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `login_log`
+--
+
+CREATE TABLE `login_log` (
+  `id` int(11) NOT NULL,
+  `ip_address` varchar(30) NOT NULL,
+  `try_time` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user_details`
 --
 
 CREATE TABLE `user_details` (
-  `Name` text NOT NULL,
+  `id` int(11) NOT NULL,
+  `Name` varchar(50) NOT NULL,
   `Email` varchar(30) NOT NULL,
   `Account_Bal` int(30) NOT NULL,
-  `Password` text NOT NULL,
+  `Password` varchar(50) NOT NULL,
   `CVV` int(4) NOT NULL,
   `Address` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -40,11 +54,43 @@ CREATE TABLE `user_details` (
 -- Dumping data for table `user_details`
 --
 
-INSERT INTO `user_details` (`Name`, `Email`, `Account_Bal`, `Password`, `CVV`, `Address`) VALUES
-('IamUtsavP', 'parekhutsav13@gmail.com', 10000, '12', 5663, 'New Ratan Apartment, S.v.road Borivali West.\\r\\n23'),
-('CharmeeM', 'charmee.m@somaiya.edu', 10000, 'charmeeuser', 3003, 'Borivali West'),
-('TestUser', 'test123@gmail.com', 10000, 'testUser', 8105, 'Borivali West'),
-('Utsav132', 'utsav.parekh@somaiya.edu', 10000, '123', 2338, 'New Ratan Apartment, S.v.road Borivali West.\\r\\n23');
+INSERT INTO `user_details` (`id`, `Name`, `Email`, `Account_Bal`, `Password`, `CVV`, `Address`) VALUES
+(1, 'IamUtsavP', 'parekhutsav13@gmail.com', 10000, '12', 5663, 'New Ratan Apartment, S.v.road Borivali West.\\r\\n23'),
+(2, 'CharmeeM', 'charmee.m@somaiya.edu', 10000, 'charmeeuser', 3003, 'Borivali West'),
+(3, 'TestUser', 'test123@gmail.com', 10000, 'testUser', 8105, 'Borivali West'),
+(4, 'Utsav132', 'utsav.parekh@somaiya.edu', 10000, '123', 2338, 'New Ratan Apartment, S.v.road Borivali West.\\r\\n23');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `login_log`
+--
+ALTER TABLE `login_log`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_details`
+--
+ALTER TABLE `user_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `login_log`
+--
+ALTER TABLE `login_log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `user_details`
+--
+ALTER TABLE `user_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
