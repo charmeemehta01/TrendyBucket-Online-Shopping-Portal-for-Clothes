d4369e426777e9bdf1073fe617bf21e20d2be7c6
Utsav Parekh <45631013+iamutsavparekh@users.noreply.github.com>
2020-10-18 15:13:11 +0530

    Login/SignUP/Forgot_Pass

--- Signup.php
+++ Signup.php
@@ -5,12 +5,29 @@
 	{      		
             $acc_bal="10000";
             $cvv=rand(1000,9999);
-    		$conn=mysqli_connect('localhost','root','','trendybucket') or die(mysqli_error());  
-            $uname=mysqli_real_escape_string($conn, $_POST['name']); 
-            $email=mysqli_real_escape_string($conn, $_POST['email']);
-            $password=mysqli_real_escape_string($conn, $_POST['pwd']);
-            $sql = "INSERT INTO user_details  VALUES ('".$uname."','".$email."','".$acc_bal."','".$password."','".$cvv."')";
-            if ($result=mysqli_query($conn,$sql)) {
+            $mysqli=new mysqli('localhost','root','','trendybucket');
+            if ($mysqli->connect_errno) {
+                echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+            } 
+            $uname=mysqli_real_escape_string($mysqli, $_POST['name']); 
+            $email=mysqli_real_escape_string($mysqli, $_POST['email']);
+            $password=mysqli_real_escape_string($mysqli, $_POST['pwd']);
+            $stmt = $mysqli->prepare("SELECT Email FROM user_details WHERE Email = ? LIMIT 1");
+            $stmt->bind_param('s',$email);
+            $stmt->execute();
+            $stmt->bind_result($email);
+            $stmt->store_result();
+            if($stmt->num_rows == 1)
+            {
+                echo "User already exists <br>";
+                echo "Click  <a href='login.html'>here</a> to go back to Login page";
+            }
+            else{
+            $balance=10000;
+
+            $sql = $mysqli->prepare("INSERT INTO user_details  VALUES (?,?,?,?,?)");
+            $sql->bind_param("ssisi",$uname,$email,$balance,$password,$cvv);
+            if ($sql->execute()) {
                     session_start();
                     $_SESSION["email"] = $email;
                     $_SESSION["acc_bal"] = $acc_bal;
@@ -27,6 +44,7 @@
                 echo "Signup failed...";
                 echo "Click  <a href='Signup.html'>here</a> to go back to signup page";
             }
+        }
     }  
     else
     {
