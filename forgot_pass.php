8083417bc07eb340d97638019eaac84d7d53036a
Utsav Parekh <45631013+iamutsavparekh@users.noreply.github.com>
2020-11-20 19:31:39 +0530

    Forgot_Password_Email

--- forgot_pass.php
+++ forgot_pass.php
@@ -50,35 +50,50 @@
     </script>
 
 <?php
-session_start();
-$conn=mysqli_connect('localhost','root','','trendybucket') or die(mysqli_error());  
-$successful='';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+require 'vendor/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/src/SMTP.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+$user_id = $_POST['user_id'];
 if(isset($_POST['submit']))
 {
-    
-    $user_id = $_POST['user_id'];
-    $result = mysqli_query($conn,"SELECT * FROM user_details where Email='" . $_POST['user_id'] . "'");
-    $row = mysqli_fetch_assoc($result);
-	$fetch_user_id=$row['Email'];
-	$password=$row['Password'];
-	if($user_id==$fetch_user_id) {
-        $password=$_POST['pwd'];
-        $sql = "UPDATE user_details SET Password='".$password."' WHERE Email='" . $_POST['user_id'] . "'";
-        $result=mysqli_query($conn,$sql);
-        if ($conn->query($sql) === TRUE) {
-            $successful="Updated successfully";   
-          } else {
-            $successful="Error updating record: " . $conn->error;
-          }
-        
-    }		
-				else{
-					echo 'invalid userid';
-				}
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'trendybucketmailer@gmail.com';                     // SMTP username
+    $mail->Password   = 'test@1234';                               // SMTP password
+    $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('trendybucketmailer@gmail.com', 'Trendy Bucket Mailer');
+    $mail->addAddress($user_id, 'Joe User');     // Add a recipient
+    $mail->addReplyTo('no-reply@gmail.com', 'No-REPLY');
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error";
+}
 }
 ?>
-
-
 
     <!-- Page Preloder -->
     <div id="preloder">
@@ -165,7 +180,6 @@ if(isset($_POST['submit']))
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" onsubmit="return validate()" method='post'>
 <table cellspacing='5' align='center'>
     <h5>Forgot Password</h5>
-    <br>
 <tr>
     <td>
         User Email: <input type='text' placeholder="Enter User Email" name='user_id'/>
@@ -173,24 +187,6 @@ if(isset($_POST['submit']))
         
     </td>
 </tr>
-<tr>
-    <td>
-    Password:<br>
-    <input type="password" id="pwd" name="pwd" name="tracking-code"
-    id="tracking-code" autocomplete="off" placeholder="Enter Password"
-    required><br>
-    </td>
-    </tr>
-    <tr>
-    <td>
-    Re-Enter Password:<br>
-    <input type="password" id="rpwd" name="rpwd" name="tracking-code"
-    id="tracking-code1" autocomplete="off" placeholder="Re-Enter Password"
-    required><br>
-    <span class="error"><?php echo $successful;?></span>
-    <br>
-    </td>
-    </tr>
 <tr><td>  <button type="Submit" name="submit" class="site-btn">Submit</button></td></tr>
 </table>
 </form>
